f4f2308940608afdd0fc53451eee2e0ad8601212
Sankalp Zurmure <Sankalp Zurmure>
2021-04-16 20:04:58 +0530

    Added django-heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,3 +147,5 @@ AZURE_ACCOUNT_KEY = os.environ.get("AZURE_ACCOUNT_KEY")
 AZURE_CONTAINER = "media"
 # AZURE_SSL = "False"
 AZURE_LOCATION = "/"
+
+django_heroku.settings(locals())
