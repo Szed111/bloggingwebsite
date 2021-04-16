ec44f790d7a60725ffee1590c6b286cefc7880f5
Sankalp Zurmure <Sankalp Zurmure>
2021-04-16 19:37:17 +0530

    updated allowedhosts,static_root,allowedhosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["learningblogweb.herokuapp.com"]
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
