8143c7c5a5482343ca2b161694869ef205557adc
Sankalp Zurmure <Sankalp Zurmure>
2021-05-19 20:19:55 +0530

    made changes to blogurl

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("about/", views.about, name="blog-about"),
+    path("contributors/", views.contributors, name="blog-contributors"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("user/<str:username>/", UserPostListView.as_view(), name="user-posts"),
